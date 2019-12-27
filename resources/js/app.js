5ade50881671d872ffa1bb7aa8959145d1177e70
uni51 <uni51@users.noreply.github.com>
2019-12-27 12:50:48 +0900

    Chapter06:認証機能とVuex　CSRF対策

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,13 @@
+import './bootstrap'
 import Vue from 'vue'
-// ルーティングの定義をインポートする
 import router from './router'
-// ストアを読み込む
 import store from './store'
-// ルートコンポーネントをインポートする
 import App from './App.vue'
 
 new Vue({
   el: '#app',
   router, // ルーティングの定義を読み込む
-  store,
+  store, // ストアを読み込む
   components: { App },  // ルートコンポーネントの使用を宣言する
   template: '<App />' // ルートコンポーネントを描画する
 })
