1467b23e0e8e6309f09ac08f85e27f72b92166bd
uni51 <uni51@users.noreply.github.com>
2019-12-27 13:34:05 +0900

    Chapter07：認証機能とVuex Part.2　認証状態を維持する

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,16 @@ import router from './router'
 import store from './store'
 import App from './App.vue'
 
-new Vue({
-  el: '#app',
-  router, // ルーティングの定義を読み込む
-  store, // ストアを読み込む
-  components: { App },  // ルートコンポーネントの使用を宣言する
-  template: '<App />' // ルートコンポーネントを描画する
-})
+const createApp = async () => {
+  await store.dispatch('auth/currentUser')
+
+  new Vue({
+    el: '#app',
+    router, // ルーティングの定義を読み込む
+    store, // ストアを読み込む
+    components: {App},  // ルートコンポーネントの使用を宣言する
+    template: '<App />' // ルートコンポーネントを描画する
+  })
+}
+
+createApp()
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ Route::post('/register', 'Auth\RegisterController@register')->name('register');
 // ログイン
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 
-
 // ログアウト
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+// ログインユーザー
+Route::get('/user', function () {
+    return Auth::user();
+})->name('user');
